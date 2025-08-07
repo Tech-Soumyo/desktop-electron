7650da698a005d291bd2f9c1be236d72b4b16ea8
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2025-08-07 14:55:14 +0530

    osUtils

--- src/electron/resourceManager.ts
+++ src/electron/resourceManager.ts
@@ -1,16 +1,54 @@
-import osUtils from "os-utils";
+import {
+  getProcesses,
+  allLoadavg,
+  cpuCount,
+  cpuFree,
+  cpuUsage,
+  freemem,
+  freememPercentage,
+  harddrive,
+  platform,
+  processUptime,
+  sysUptime,
+  totalmem,
+} from "os-utils";
 
 const Polling_Interval = 500;
 
-export const pollresources = () => {
+export const pollresources = async () => {
+  const [freeCpu, processes] = await Promise.all([
+    new Promise((resolve) => getProcesses(resolve)),
+    new Promise((resolve) => cpuFree(resolve)),
+    // new Promise((resolve) => harddrive(resolve)),
+  ]);
+
   setInterval(async () => {
     const cpuUsage = await getCpuUsage();
-    console.log(cpuUsage);
+    console.log({ cpuUsage });
+    console.log(
+      JSON.stringify(
+        {
+          platform: platform(),
+          cpuCount: cpuCount(),
+          cpuFree: freeCpu,
+          allLoadavg: allLoadavg(),
+          freemem: freemem(),
+          freememPercent: freememPercentage(),
+          totalmem: totalmem(),
+          sysUptime: sysUptime(),
+          processUptime: processUptime(),
+          // harddrive: driveInfo,
+          processes: processes,
+        },
+        null,
+        2
+      )
+    );
   }, Polling_Interval);
 };
 
 const getCpuUsage = () => {
   return new Promise((resolve) => {
-    osUtils.cpuUsage(resolve);
+    cpuUsage(resolve);
   });
 };
