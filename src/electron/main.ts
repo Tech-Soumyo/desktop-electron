081487b37fca763c6ef5628dcc524f4977eccb47
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2025-08-07 13:38:05 +0530

    all the setup for running electron seemlessly

---
+++ src/electron/main.ts
@@ -0,0 +1,17 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+import { isDev } from "./util.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({});
+
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "..", "dist-react", "index.html"));
+  }
+});
