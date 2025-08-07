605c48d3269bf89308ed2018206bfb6a1f5f0114
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2025-08-07 23:43:37 +0530

    how to comunicate with UI

--- src/ui/vite-env.d.ts
+++ src/ui/vite-env.d.ts
@@ -1,1 +1,29 @@
 /// <reference types="vite/client" />
+
+export interface IStatistics {
+  cpuUsage: number;
+  netStorage: {
+    total: number;
+    usage: number;
+  };
+  platform: string;
+  cpuCount: number;
+  cpuFree: number;
+  allLoadavg: string;
+  freemem: number;
+  freememPercent: number;
+  totalmem: number;
+  sysUptime: number;
+  processUptime: number;
+  processes: string[];
+}
+export interface IElectronAPI {
+  subscribeStatistics: (callback: (statistics: IStatistics) => void) => void;
+  getStaticData: () => void;
+}
+
+declare global {
+  interface Window {
+    electron: IElectronAPI;
+  }
+}
