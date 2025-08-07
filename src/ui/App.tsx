1d4580c919d9a0aae6a70d47ecce4043a31e51f4
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2025-08-07 23:43:33 +0530

    how to comunicate with UI

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,10 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import type { IStatistics } from "./vite-env";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [latestStats, setLatestStats] = useState<IStatistics | null>(null);
+  const [cpuUsageHistory, setCpuUsageHistory] = useState<number[]>([]);
+
+  // fetching the data from Electron js Backend
+  useEffect(() => {
+    window.electron.getStaticData();
+    window.electron.subscribeStatistics((stats) => {
+      setLatestStats(stats);
+      // console.log(stats);
+    });
+  }, []);
+
+  const handleShowCpuUsage = () => {
+    if (latestStats?.cpuUsage) {
+      setCpuUsageHistory((prevHistory) => [
+        ...prevHistory,
+        latestStats.cpuUsage,
+      ]);
+    }
+  };
 
   return (
     <>
@@ -22,10 +43,21 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={handleShowCpuUsage}>Show Current CPU Usage</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      {cpuUsageHistory.length > 0 && (
+        <div>
+          <h3>CPU Usage History (on click):</h3>
+          <ul>
+            {cpuUsageHistory.map((usage, index) => (
+              <li key={index}>{(usage * 100).toFixed(2)}%</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
